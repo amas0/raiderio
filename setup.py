239d275b2c3e40cc72ec39d5baf83718312c7669
amas <andrew.mascioli1@gmail.com>
2023-10-18 20:45:38 -0400

    Update package version 1.1 -> 1.1.1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='raiderio',
-      version='1.1',
-      author='amas0',
-      description='Python client library for raider.io\'s public API',
-      packages=find_packages(),
-      install_requires=[
-            'requests'
-      ])
+setup(
+    name="raiderio",
+    version="1.1.1",
+    author="amas0",
+    description="Python client library for raider.io's public API",
+    packages=find_packages(),
+    install_requires=["requests"],
+)
